968fef44a7b0d24339e3a04ce843533e1c16e070
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-03-30 20:35:33 -0300

    refactor: Get regex from appropriate file

--- test/unit/generators/NameGenerator.spec.ts
+++ test/unit/generators/NameGenerator.spec.ts
@@ -1,42 +1,42 @@
 import NameGenerator from "../../../src/generators/credentials/NameGenerator"
+import {
+  regexFirstName,
+  regexFullName,
+  regexUserName,
+} from "../../../src/utils/Regex"
 
 describe("NameGenerator", () => {
   describe("generateRandomName", () => {
     it("should generate a random username", () => {
       const username = NameGenerator.generateRandomName({ type: "username" })
-      const regex = /^[a-z0-9]+$/
       expect(username).toBeDefined()
-      expect(regex.test(username)).toBe(true)
+      expect(regexUserName.test(username)).toBe(true)
     })
 
     it("should generate a random first name", () => {
       const firstName = NameGenerator.generateRandomName({ type: "firstName" })
-      const regex = /^[A-Za-z]+$/
       expect(firstName).toBeDefined()
-      expect(regex.test(firstName)).toBe(true)
+      expect(regexFirstName.test(firstName)).toBe(true)
     })
 
     it("should generate a random full name", () => {
       const fullName = NameGenerator.generateRandomName({ type: "fullName" })
-      const regex = /^[a-zA-Z]+(?:\s+[a-zA-Z]+)+$/
       expect(fullName).toBeDefined()
-      expect(regex.test(fullName)).toBe(true)
+      expect(regexFullName.test(fullName)).toBe(true)
     })
 
     it("should generate a random first name when type is not specified", () => {
       const firstName = NameGenerator.generateRandomName()
-      const regex = /^[A-Za-z]+$/
       expect(firstName).toBeDefined()
-      expect(regex.test(firstName)).toBe(true)
+      expect(regexFirstName.test(firstName)).toBe(true)
     })
 
     it("should generate a random first name for male gender", () => {
       const firstName = NameGenerator.generateRandomName({
         gender: "male",
       })
-      const regex = /^[A-Za-z]+$/
       expect(firstName).toBeDefined()
-      expect(regex.test(firstName)).toBe(true)
+      expect(regexFirstName.test(firstName)).toBe(true)
     })
 
     it("should generate a random first name for female gender", () => {
