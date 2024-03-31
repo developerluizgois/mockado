0e34d40bfb800ec4695de9dcdd852a29d39cbc5e
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-03-31 02:56:39 -0300

    feat: add regex to validate date formats

--- src/utils/Regex.ts
+++ src/utils/Regex.ts
@@ -7,3 +7,10 @@ export const regexPasswordWithLetters = /^[a-z]+$/
 export const regexPasswordWithCapitalLetters = /^[A-Z]+$/
 export const regexPasswordWithNumbers = /^[0-9]+$/
 export const regexPasswordWithSpecialCharacters = /^[!@#$%^&*()_\-+=<>?]+$/
+export const regexYYYY_MM_DD = /^\d{4}-\d{2}-\d{2}$/
+export const regexDD_MM_YYYY = /^\d{2}\/\d{2}\/\d{4}$/
+export const regexMM_DD_YYYY = /^\d{2}\/\d{2}\/\d{4}$/
+export const regexMMM_DD_YYYY = /^[a-zA-Z]{3}\s\d{2},\s\d{4}$/
+export const regexDD_MMM_YYYY = /^\d{2}\s[a-zA-Z]{3}\s\d{4}$/
+export const regexYYYY_MM_DD_Slash = /^\d{4}\/\d{2}\/\d{2}$/
+export const regexYYYY_MM_DD_HH_mm_ss = /^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}$/
