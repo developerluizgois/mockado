cd55f9984b6d6d7fb527bb6fe3d86efc3f791bc5
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-03-31 23:47:52 -0300

    feat: export random address generator

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import EmailGenerator from "./generators/credentials/EmailGenerator"
 import PasswordGenerator from "./generators/credentials/PasswordGenerator"
 import DateGenerator from "./generators/personal/Date"
 import IdentificationGenerator from "./generators/personal/Identification"
+import PhoneNumberGenerator from "./generators/contact/PhoneGenerator"
+import LocationGenerator from "./generators/contact/LocationGenerator"
 
 const mockado = {
   name: NameGenerator.generateRandomName,
@@ -10,6 +12,8 @@ const mockado = {
   password: PasswordGenerator.generateRandomPassword,
   date: DateGenerator.generateRandomDOB,
   identification: IdentificationGenerator.generateRandomIdentification,
+  phone: PhoneNumberGenerator.generateRandomPhoneNumber,
+  location: LocationGenerator.generateRandomAddress,
 }
 
 export default mockado
