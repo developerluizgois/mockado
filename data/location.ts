b4c1bced869c4f68e2e25c5a45176859166d740d
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-03-31 23:46:14 -0300

    feat: add address array

---
+++ data/location.ts
@@ -0,0 +1,83 @@
+interface ObjectAdresses {
+  street: string
+  number: string
+  district: string
+  city: string
+  state: string
+  zipCode: string
+}
+
+export const adresses: ObjectAdresses[] = [
+  {
+    street: "Av. Atlântica",
+    number: "1702",
+    district: "Copacabana",
+    city: "Rio de Janeiro",
+    state: "Rio de Janeiro",
+    zipCode: "22021-001",
+  },
+  {
+    street: "Av. Paulista",
+    number: "1578",
+    district: "Bela Vista",
+    city: "São Paulo",
+    state: "São Paulo",
+    zipCode: "01310-200",
+  },
+  {
+    street: "Praça XV de Novembro",
+    number: "24",
+    district: "Centro",
+    city: "Rio de Janeiro",
+    state: "Rio de Janeiro",
+    zipCode: "20010-010",
+  },
+  {
+    street: "Av. Boa Viagem",
+    number: "214",
+    district: "Boa Viagem",
+    city: "Recife",
+    state: "Pernambuco",
+    zipCode: "51011-000",
+  },
+  {
+    street: "Av. das Cataratas",
+    number: "12450",
+    district: "Vila Yolanda",
+    city: "Foz do Iguaçu",
+    state: "Paraná",
+    zipCode: "85853-000",
+  },
+  {
+    street: "Eixo Monumental",
+    number: "58",
+    district: "Asa Sul",
+    city: "Brasília",
+    state: "Distrito Federal",
+    zipCode: "70070-200",
+  },
+  {
+    street: "Praia de Iracema",
+    number: "68",
+    district: "Praia de Iracema",
+    city: "Fortaleza",
+    state: "Ceará",
+    zipCode: "60060-000",
+  },
+  {
+    street: "Praça da Sé",
+    number: "34",
+    district: "Sé",
+    city: "São Paulo",
+    state: "São Paulo",
+    zipCode: "01001-000",
+  },
+  {
+    street: "Estr. das Paineiras",
+    number: "1",
+    district: "Alto da Boa Vista",
+    city: "Rio de Janeiro",
+    state: "Rio de Janeiro",
+    zipCode: "22241-330",
+  },
+]
